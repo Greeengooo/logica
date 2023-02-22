644f10648e12675e673e6a762d8d606a306ab370
Greeengooo <oanoshyn@zscaler.com>
2023-02-22 16:39:59 +0100

    test

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='logica',
+    version='1.3.141',
+    author='PolicyEngine team',
+    author_email='',
+    url='',
+    description='logica',
+    packages=["common", "compiler", "parser_py"],
+    python_requires='>=3.6',
+)
